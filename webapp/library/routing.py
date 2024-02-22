3fad8b170d7bda06e356676fe5bf71d116b7d33e
Adriano Giuliani <99326204+AdriIT@users.noreply.github.com>
2024-02-22 18:25:14 +0100

    Delete webapp directory

--- webapp/library/routing.py
+++
@@ -1,9 +0,0 @@
-#routing.py
-
-from django.urls import re_path
-
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r"ws/devices/(?P<device_id>\w+)/$", consumers.ChatConsumer.as_asgi()),
-]
